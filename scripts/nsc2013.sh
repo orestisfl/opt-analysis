d8f1bb5deef122b3b3a3427f39a57adeb5d86a81
Orestis Floros <forestis@kth.se>
2020-06-01 10:21:17 +0200

    tmp

--- scripts/nsc2013.sh
+++ scripts/nsc2013.sh
@@ -19,7 +19,7 @@ clang-10 Deadpool/wbs_aes_nsc2013_variants/target/nosuchcon_2013_whitebox_noenc_
 clang-10 -c -emit-llvm -O0 -Xclang -disable-O0-optnone Deadpool/wbs_aes_nsc2013_variants/target/nosuchcon_2013_whitebox_noenc.c -o /tmp/base.bc
 
 # Optimize the base bitcode with the current optimization flag.
-opt-10 "-$OPT_TO_USE" /tmp/base.bc -o /tmp/tmp.bc
+eval opt-10 "-$OPT_TO_USE" /tmp/base.bc -o /tmp/tmp.bc
 clang-10 /tmp/tmp.bc -o /tmp/target
 
 mkdir out
