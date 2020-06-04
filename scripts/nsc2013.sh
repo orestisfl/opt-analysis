971f97fe153504242eef165a8300f38051b1a53f
Orestis Floros <forestis@kth.se>
2020-06-04 15:06:47 +0200

    tmp
    - Get opts from file
    - No 100 inputs
    - Add generated file

--- scripts/nsc2013.sh
+++ scripts/nsc2013.sh
@@ -7,25 +7,28 @@ set -x
 [[ -z $OPT_BATCH ]] && exit 1
 [[ -z $OPT_RUNS ]] && exit 1
 
+# https://stackoverflow.com/a/6022431
+opts="$(sed "${OPT_TO_USE}q;d" opts.conf)"
+
 experiments=25
 mkdir -p results
 
 clang-10 --version
 clang-10 Deadpool/wbs_aes_nsc2013_variants/target/nosuchcon_2013_whitebox_noenc_generator.c -o /tmp/generator
-/tmp/generator
 [[ -f wbt_noenc ]]
 
 # Create the bitcode of the file to be optimized
 clang-10 -c -emit-llvm -O0 -Xclang -disable-O0-optnone Deadpool/wbs_aes_nsc2013_variants/target/nosuchcon_2013_whitebox_noenc.c -o /tmp/base.bc
 
 # Optimize the base bitcode with the current optimization flag.
-eval opt-10 "-$OPT_TO_USE" /tmp/base.bc -o /tmp/tmp.bc
+eval opt-10 "-$opts" /tmp/base.bc -o /tmp/tmp.bc
 clang-10 /tmp/tmp.bc -o /tmp/target
 
+(
 mkdir out
 cd out
 cp ../wbt_noenc ./
-opt-analysis -j --daredevil --progress --inputs $OPT_RUNS --experiments $experiments nsc2013 /tmp/target
+opt-analysis -j --daredevil --progress --inputs "$OPT_RUNS" --experiments $experiments nsc2013 /tmp/target
+)
 
-cd ..
-zip -r "results/$OPT_BATCH.O-$OPT_TO_USE-$experiments-$OPT_RUNS.zip" out/
+zip -r "results/$OPT_BATCH.O$OPT_TO_USE-$experiments-$OPT_RUNS.zip" out/
